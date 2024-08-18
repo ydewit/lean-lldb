36695b284a20df72cb8cde26346971df47b06647
Yuri de Wit <ydewit@gmail.com>
2024-08-18 10:53:48 -0300

    initial github actions

--- formatters/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-from . import lean
-
-log = logging.getLogger(__name__)
-
-
-def __lldb_init_module(debugger_obj, internal_dict):  # pyright: ignore
-    log.info('Initializing')
-    lean.__lldb_init_module(debugger_obj, internal_dict)
